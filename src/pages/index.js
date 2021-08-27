ccea49c1cb48c46a73143d4d0b8aa923fb24f21e
Joseph M <jmedina1985@hotmail.com>
2021-08-27 16:29:13 -0700

    getting started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  // just dumping images into static folder and using from there is discouraged - just because you can doesn't mean you should
+
   return <div>    
-    <img src="/3-puppy.jpg" alt=""></img>
+
     Hello world!</div>
 }
