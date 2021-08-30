ec2b755ffe29bdd96412959d5d5184c3345e2cdf
Joseph M <jmedina1985@hotmail.com>
2021-08-30 14:08:31 -0700

    layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const about = () => {
   return (
-    <div>
+    <Layout>
 
       <h2>hello from about page</h2>
-    </div>
+    </Layout>
   )
 }
 
