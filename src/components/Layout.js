1eb2ade4d6b0cd7c356882d8556602e0bd2eaf32
Joseph M <jmedina1985@hotmail.com>
2021-08-30 14:03:05 -0700

    layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
