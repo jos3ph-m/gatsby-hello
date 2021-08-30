c967cb709ed79fc66464d3f288163c7dd3b6e0ed
Joseph M <jmedina1985@hotmail.com>
2021-08-30 14:05:22 -0700

    layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 
 const about = () => {
   return (
     <div>
-      <Navbar />
+
       <h2>hello from about page</h2>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
 import {Link} from 'gatsby'
-import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
   <div>
-    <Navbar />
+
     <h1>    
       hello people</h1>
       <div>
