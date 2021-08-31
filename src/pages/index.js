9b6f074f7a3549c8111577b0b6ef278a35ccad83
Joseph M <jmedina1985@hotmail.com>
2021-08-31 14:32:13 -0700

    Some styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,10 @@ export default function Home() {
   return (
   <Layout>
     <div className="home-header">
-
-    </div>
-
     <h1>    
       Home page</h1>
-      <div>
-    <Link to="/about">About</Link>
     </div>
-      <div>
-    <Link to="/company/history">History</Link>
-    </div>
+   
   </Layout>
   )
 }
