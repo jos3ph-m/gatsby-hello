9a159cb536d42f001ab309e08c00a2453e086569
Joseph M <jmedina1985@hotmail.com>
2021-08-31 14:25:36 -0700

    Some styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,9 @@ const Navbar = () => {
         <li>
         <Link to="/about">about</Link>
         </li>
+        <li>
+        <Link to="/contact">contact</Link>
+        </li>
       </ul>
     </nav>
   )
