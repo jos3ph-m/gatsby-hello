e5394c88b78340c51fba28bd05a175592b6c6f53
Joseph M <jmedina1985@hotmail.com>
2021-09-01 14:06:00 -0700

    Some styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,9 @@ import { page, text } from '../examples/about.module.css'
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={page}>
       <h1>about page</h1>
-      <p className={text}></p>
+      <p className={text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, earum.</p>
       </div>
       
     </Layout>
