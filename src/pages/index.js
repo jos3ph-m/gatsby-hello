2b016453b4beb6cd54aa36f7d4ef7f9a2d4544f8
Joseph M <jmedina1985@hotmail.com>
2021-09-01 14:01:34 -0700

    Some styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import { page, text } from '../examples/home.module.css'
 
 export default function Home() {
   return (
