599d2cff41365735b0eeb79f2dff80535707744f
Joseph M <jmedina1985@hotmail.com>
2021-09-01 14:02:53 -0700

    Some styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,11 @@ import { page, text } from '../examples/home.module.css'
 export default function Home() {
   return (
   <Layout>
-    <div className="home-header">
-    <h1>    
-      Home page</h1>
+    <div className={page}>
+    <h1>Home page</h1>
+    <h1>Hello page</h1>
+    <h1>Home hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam illo sint laudantium ipsum. Dolores beatae, assumenda, perspiciatis aliquid corrupti, cum et necessitatibus expedita esse repudiandae voluptas. Provident illum nulla ducimus. Quisquam nisi assumenda beatae vero amet ipsa aspernatur laudantium deleniti autem saepe! Corporis cupiditate sint veniam dolores officiis hic vero!</p>
     </div>
    
   </Layout>
