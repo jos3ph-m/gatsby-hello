d83b56b95e7ae1a7d8ea225df533cfe8cbc8c6b5
Joseph M <jmedina1985@hotmail.com>
2021-09-02 13:56:05 -0700

    Some styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
-import './layout.css'
-
 const Layout = ({children}) => {
   
   return (
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,31 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import styled from 'styled-components';
+
 
 const about = () => {
   return (
     <Layout>
-      <Wrapper>
+      
       <h1>about page</h1>
       <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, earum.</p>
-      </Wrapper>
-      
+
     </Layout>
   )
 }
 
-const Wrapper = styled.section`
-
-  color: pink;
-
-
-h1 {
-  color: yellowgreen;
-}
-
-.text{
-  text-transform: uppercase;
-}
-`
 
 export default about
