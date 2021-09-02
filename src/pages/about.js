ae389c3d98937761331b56f62e9ea42a69ecadb7
Joseph M <jmedina1985@hotmail.com>
2021-09-02 13:56:30 -0700

    Some styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,6 @@ const about = () => {
     <Layout>
       
       <h1>about page</h1>
-      <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, earum.</p>
-
     </Layout>
   )
 }
