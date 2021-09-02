3ab48d3a8efa9ab966db7ad1849647fac6e42bca
Joseph M <jmedina1985@hotmail.com>
2021-09-02 13:49:53 -0700

    Some styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import * as aboutStyles from '../examples/about.module.css'
 import styled from 'styled-components';
 
 const about = () => {
