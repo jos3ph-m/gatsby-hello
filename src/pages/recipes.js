14eae65587b6186d9f4610c398718077fe77d054
Joseph M <jmedina1985@hotmail.com>
2021-09-03 16:05:17 -0700

    Recipes page

---
+++ src/pages/recipes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+
+const recipes = () => {
+  return (
+    <Layout>
+      <h1>about page</h1>
+    </Layout>
+  )
+}
+
+
+export default recipes
