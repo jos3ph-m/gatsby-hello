39455492f270c28720e1b0f27d979d3979e866d7
Joseph M <jmedina1985@hotmail.com>
2021-09-03 16:06:24 -0700

    Recipes page

---
+++ src/pages/tags.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+
+const Tags = () => {
+  return (
+    <Layout>
+      <h1>Tags page</h1>
+    </Layout>
+  )
+}
+
+
+export default Tags
