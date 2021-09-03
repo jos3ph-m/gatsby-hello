6e45b12f712beee46665e6dfd49d9fcfb0637d49
Joseph M <jmedina1985@hotmail.com>
2021-09-03 16:09:48 -0700

    Fixing error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
+
 const Error = () => {
   return (
-    <div>
-      Error page
-    </div>
+    <Layout>
+      <h1>Error page</h1>
+    </Layout>
   )
 }
 
