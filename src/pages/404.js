881cd3fa064eaafa19bb7e92bdf8a2331c2c80cc
Joseph M <jmedina1985@hotmail.com>
2021-09-04 14:32:37 -0700

    Working on error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,8 @@ const Error = () => {
     <Layout>
       <main className="error-page">
         <section>
-          <h1></h1>
+          <h1>404</h1>
+          <h3>page not found</h3>
         </section>
       </main>
     </Layout>
