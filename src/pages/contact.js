45c7113e2d3bdb1abf99ffa3aafb1de1f7f5695e
Joseph M <jmedina1985@hotmail.com>
2021-09-04 15:15:23 -0700

    Working on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,7 +14,7 @@ const Contact = () => {
           <p>Synth kogi letterpress church-key activated charcoal man bun typewriter fugiat mollit. Ut DIY prism stumptown irony. Gastropub tbh distillery non. Photo booth tacos craft beer, cornhole aesthetic cold-pressed nulla glossier. Art party iPhone vexillologist, pickled scenester selfies next level marfa tote bag velit taxidermy readymade slow-carb adaptogen cupidatat.</p>
           </article>
           <article>
-            
+            <form action="" className="form contact-form"></form>
           </article>
         </section>
       </main>
