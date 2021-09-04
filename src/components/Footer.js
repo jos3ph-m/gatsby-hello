4671b8b09bf8359c24559cc746fdff6d760eb92a
Joseph M <jmedina1985@hotmail.com>
2021-09-04 14:27:09 -0700

    Working on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="page-footer">
-      
+      <p>
+        &copy; 2021 <span>SimplyRecipes</span> Built with <a href=""></a>
+      </p>
     </footer>
   )
 }
