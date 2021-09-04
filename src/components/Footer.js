b8ab1561f27a06e74562b04a1d57edcd0c88a2e1
Joseph M <jmedina1985@hotmail.com>
2021-09-04 14:27:45 -0700

    Working on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="page-footer">
       <p>
-        &copy; 2021 <span>SimplyRecipes</span> Built with <a href=""></a>
+        &copy; 2021 <span>SimplyRecipes</span> Built with <a href="https://www.gatsbyjs.com/">Gatsby</a>
       </p>
     </footer>
   )
