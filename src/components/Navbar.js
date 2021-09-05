0a197d953bd8d50192e2cbeaea3af30dd161b8ef
Joseph M <jmedina1985@hotmail.com>
2021-09-05 15:06:37 -0700

    Working on the nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import { FiAlignJustify } from 'react-icons/fi'
+import logo from '../assets/images/logo.svg'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header"></div>
+        <div className="nav-links show-links"></div>
       </div>
       
     </nav>
