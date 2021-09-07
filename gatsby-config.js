8ae04c07736550f1d24182bb28a42e43e696d230
Joseph M <jmedina1985@hotmail.com>
2021-09-07 15:52:07 -0700

    gatsby images

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      
-      options: {
-        // Add any options here
-      },
-    },
+    `gatsby-plugin-styled-components`,
+
   ],
 }
