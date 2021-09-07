3c4972f4907426b529b51672182be63cfeb7e483
Joseph M <jmedina1985@hotmail.com>
2021-09-07 16:04:42 -0700

    gatsby images

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import {StaticImage} from 'gatsby-plugin-image'
+import {Link} from 'gatsby'
 
 
 const About = () => {
   return (
     <Layout>
-      <h1>About page</h1>
+      
     </Layout>
   )
 }
