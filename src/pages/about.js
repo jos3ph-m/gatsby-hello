e3a5b6fc3342dd6dd42d63f515f180e8f7c1af5e
Joseph M <jmedina1985@hotmail.com>
2021-09-08 14:05:35 -0700

    StaticImage

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import {StaticImage} from 'gatsby-plugin-image'
-import {Link} from 'gatsby'
-
+import React from "react"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
@@ -11,16 +10,20 @@ const About = () => {
         <section className="about-page">
           <article>
             <h2>I'm a baby coloring book baby</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cum neque distinctio quo nemo nisi!</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+              cum neque distinctio quo nemo nisi!
+            </p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <Link to="/contact" className="btn">contact</Link>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
           </article>
-          <StaticImage
+          <StaticImage src="" />
         </section>
       </main>
     </Layout>
   )
 }
-
 
 export default About
