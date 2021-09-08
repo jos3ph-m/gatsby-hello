6a6789e766884732db76573baeac119621b0eb9a
Joseph M <jmedina1985@hotmail.com>
2021-09-08 14:21:43 -0700

    StaticImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-
-import Layout from '../components/Layout'
-
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
-  <Layout>
-    <h1>home page</h1>
-  </Layout>
+    <Layout>
+      <h1>home page</h1>
+    </Layout>
   )
 }
