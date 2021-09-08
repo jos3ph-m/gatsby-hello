bfeddd3e0eac381f6699a09fd0431c9138f37741
Joseph M <jmedina1985@hotmail.com>
2021-09-08 14:22:09 -0700

    StaticImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import { StaticImage } from "gatsby-plugin-image"
 export default function Home() {
   return (
     <Layout>
-      <h1>home page</h1>
+      <main className="page">
+        <header className="hero"></header>
+      </main>
     </Layout>
   )
 }
