823e8b40c70f67435e6967f2d08e7a461fd3a8ab
Joseph M <jmedina1985@hotmail.com>
2021-09-13 14:59:12 -0700

    SOURCE-FILESYSTEM plugin setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,12 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `styles`,
+        path: `${__dirname}/src/assets/css`,
+      },
+    },
   ],
 }
