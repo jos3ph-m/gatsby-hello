e4f1efa44d1c5d8ac1a51a0a67a65134ad11708d
Joseph M <jmedina1985@hotmail.com>
2021-09-13 14:47:51 -0700

    SOURCE-FILESYSTEM plugin setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `pages`,
+        name: `images`,
         path: `${__dirname}/src/pages/`,
       },
     },
