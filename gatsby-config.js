5e134f63e62f6df58d4196fd651cb8f9103dd011
Joseph M <jmedina1985@hotmail.com>
2021-09-13 15:25:44 -0700

    sourceinstanceName - argument

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,12 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }
