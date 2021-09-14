27b4b0b6a64cf0d7a8603d84a923028bd7024e1e
Joseph M <jmedina1985@hotmail.com>
2021-09-14 14:52:19 -0700

    sourceinstanceName - argument

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
-import FetchData from "../examples/fetchData"
 
 export default function Home() {
   return (
@@ -22,7 +21,6 @@ export default function Home() {
             </div>
           </div>
         </header>
-        <FetchData />
       </main>
     </Layout>
   )
