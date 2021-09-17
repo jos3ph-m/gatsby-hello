65af0ab5baed95bbcf9d235606573a33ae5d93c6
Joseph M <jmedina1985@hotmail.com>
2021-09-17 14:08:55 -0700

    render gallery

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import styled from "styled-components"
 
 const query = graphql`
