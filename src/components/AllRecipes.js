90162d3c45e71ad2928f6d315655d08247b9fc6b
Joseph M <jmedina1985@hotmail.com>
2021-09-18 18:58:41 -0700

    AllRecipes component

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,11 +1,13 @@
 import React from "react"
 import TagsList from "./TagsList"
+import RecipesList.js from './RecipesList'
 
 const AllRecipes = () => {
   return (
     <div>
       <h4>all recipes</h4>
       <TagsList />
+      <RecipesList />
     </div>
   )
 }
