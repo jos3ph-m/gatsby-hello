188114bc3ebe4e7f8e7362cc207259e263009106
Joseph M <jmedina1985@hotmail.com>
2021-09-19 15:43:30 -0700

    AllRecipes component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
+import AllRecipes from "../components/AllRecipes"
 
 export default function Home() {
   return (
