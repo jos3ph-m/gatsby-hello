9e4c717c9ccd253a89cbd428d8f601536715923f
Joseph M <jmedina1985@hotmail.com>
2021-09-20 13:22:29 -0700

    RecipesList component

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -3,7 +3,13 @@ import { Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 const RecipesList = ({ recipes = [] }) => {
-  return <div className="recipes-list">this is recipeslist</div>
+  return (
+    <div className="recipes-list">
+      {recipes.map(recipe => {
+        return <p>{recipe.title}</p>
+      })}
+    </div>
+  )
 }
 
 export default RecipesList
