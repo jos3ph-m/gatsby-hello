a85fe3e242b415598d0e45c0cc89a741fa3749de
Joseph M <jmedina1985@hotmail.com>
2021-09-21 13:30:31 -0700

    featured recipes

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import { Link, graphql } from "gatsby"
+import RecipesList from "../components/RecipesList"
 
 const About = () => {
   return (
