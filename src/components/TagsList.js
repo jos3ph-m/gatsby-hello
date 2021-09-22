e3398e47cda6beda24adbd0ac4bfd9b9ad0298d7
Joseph M <jmedina1985@hotmail.com>
2021-09-22 16:48:55 -0700

    utils setup

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -2,7 +2,7 @@ import React from "react"
 import setupTags from "../utils/setupTags"
 
 const TagsList = ({ recipes }) => {
-  console.log(recipes)
+  setupTags(recipes)
   return <div>this is tagslist</div>
 }
 
