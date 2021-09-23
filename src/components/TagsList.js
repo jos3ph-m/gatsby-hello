6178bf5f51985dcfb15918e348ad29500f83da84
Joseph M <jmedina1985@hotmail.com>
2021-09-23 16:50:16 -0700

    TagsList

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -3,7 +3,7 @@ import setupTags from "../utils/setupTags"
 
 const TagsList = ({ recipes }) => {
   const newTags = setupTags(recipes)
-  return <div>this is tagslist</div>
+  return <div className="tag-container">this is tagslist</div>
 }
 
 export default TagsList
