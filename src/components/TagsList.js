20ee8eb8f891c82b21d51cbf3ca71937aa2991e0
Joseph M <jmedina1985@hotmail.com>
2021-09-23 16:51:01 -0700

    TagsList

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -6,6 +6,11 @@ const TagsList = ({ recipes }) => {
   return (
     <div className="tag-container">
       <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          return
+        })}
+      </div>
     </div>
   )
 }
