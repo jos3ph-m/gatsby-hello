e071f64fe6d6d30fbdbd244b665d035a11646dbf
Joseph M <jmedina1985@hotmail.com>
2021-09-25 16:35:46 -0700

    Recipe template page setup

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -5,6 +5,7 @@ const RecipeTemplate = () => {
     <div>
       <h4>A recipe will live here</h4>
       <h5>Which one will it be?</h5>
+      <h6>Who knows, this is just a default page</h6>
     </div>
   )
 }
