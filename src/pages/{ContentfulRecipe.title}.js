fbc0e908d40b4d5eb467eb3d973793c2ba3074c7
Joseph M <jmedina1985@hotmail.com>
2021-09-26 11:41:24 -0700

    recipe template page

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,12 +1,9 @@
 import React from "react"
 
 const RecipeTemplate = props => {
-  console.log(props)
   return (
     <div>
-      <h4>A recipe will live here...</h4>
-      <h5>...which one will it be?</h5>
-      <h6>Who knows, this is just a default template we set up.</h6>
+      <h2>{props.params.title}</h2>
     </div>
   )
 }
