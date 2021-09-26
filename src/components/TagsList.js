d4e21dab2cf7b4229cb87f349b4fddd6a65c6c06
Joseph M <jmedina1985@hotmail.com>
2021-09-26 11:57:22 -0700

    slugify

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,7 +1,6 @@
 import React from "react"
 import setupTags from "../utils/setupTags"
 import { Link } from "gatsby"
-import slugify from "slugify"
 
 const TagsList = ({ recipes }) => {
   const newTags = setupTags(recipes)
