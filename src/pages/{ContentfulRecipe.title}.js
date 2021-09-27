11c061f864f3da34c9aee3372eb0fa551c727b01
Joseph M <jmedina1985@hotmail.com>
2021-09-27 16:59:26 -0700

    Recipe template query

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -10,6 +10,25 @@ const RecipeTemplate = props => {
   )
 }
 
-export const query = graphql``
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
 
 export default RecipeTemplate
