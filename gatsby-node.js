beb3f666b79e442671a9d72299cabe15783b9ac6
Joseph M <jmedina1985@hotmail.com>
2021-09-30 15:55:34 -0700

    gatsby-node setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,15 @@
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const result = await graphql(``)
+  const result = await graphql(`
+    query GetRecipes {
+      allContentfulRecipe {
+        nodes {
+          content {
+            tags
+          }
+        }
+      }
+    }
+  `)
 }
