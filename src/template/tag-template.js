bbe375aaecaea19f4db51c4365a8931d1126d43b
Joseph M <jmedina1985@hotmail.com>
2021-09-30 15:41:07 -0700

    gatsby-node setup

--- src/template/tag-template.js
+++ src/template/tag-template.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 const TagTemplate = () => {
-  return <div></div>
+  return (
+    <div>
+      <h2>tag template page</h2>
+    </div>
+  )
 }
 
 export default TagTemplate
