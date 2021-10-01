ced041655e1e2b7eb082b6bec714e9d8c532158e
Joseph M <jmedina1985@hotmail.com>
2021-10-01 16:59:17 -0700

    tag template return

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -10,4 +10,23 @@ const TagTemplate = props => {
   )
 }
 
+export const query = graphql`
+  query GetRecipeByTag($tag: String) {
+    allContentfulRecipe(
+      filter: { content: { tags: { eq: $tag } } }
+      sort: { fields: title, order: ASC }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
+
 export default TagTemplate
