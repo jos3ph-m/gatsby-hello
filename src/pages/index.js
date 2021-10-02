5349697a671c7d61bfd7c01c116384128958befb
Joseph M <jmedina1985@hotmail.com>
2021-10-01 17:04:13 -0700

    tag template return

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
           <div className="hero-container">
             <div className="hero-text">
               <h1>simply recipes</h1>
-              <h4>no fluff, just recipes</h4>
+              <h4>no bloat</h4>
             </div>
           </div>
         </header>
