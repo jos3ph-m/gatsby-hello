25d26eb1c215c6c164192a9a0e5427f5c3d2ea16
Joseph M <jmedina1985@hotmail.com>
2021-10-02 15:03:09 -0700

    fixing bug

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,6 +1,7 @@
 import React from "react"
 import setupTags from "../utils/setupTags"
 import { Link } from "gatsby"
+import slugify from "slugify"
 
 const TagsList = ({ recipes }) => {
   const newTags = setupTags(recipes)
