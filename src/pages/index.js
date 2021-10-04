54d851e2f4d63a6689a3b03f45e686cf73b43e2c
Joseph M <jmedina1985@hotmail.com>
2021-10-04 16:02:49 -0700

    SEO optimization

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import AllRecipes from "../components/AllRecipes"
-import SEO from "../components"
+import SEO from "../components/SEO"
 
 export default function Home() {
   return (
