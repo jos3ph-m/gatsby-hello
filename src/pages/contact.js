eeb22da7456dce953396349947cd090adb4d036c
Joseph M <jmedina1985@hotmail.com>
2021-10-04 15:33:14 -0700

    Contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import RecipesList from "../components/RecipesList"
 
 const Contact = ({ data }) => {
