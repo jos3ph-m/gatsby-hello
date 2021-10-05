40abb7f059288c9f5e18e1a56d61fb15fca87a42
Joseph M <jmedina1985@hotmail.com>
2021-10-05 15:53:25 -0700

    SEO optimization

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/SEO"
 export default function Home() {
   return (
     <Layout>
-      <SEO />
+      <SEO title />
       <main className="page">
         <header className="hero">
           <StaticImage
