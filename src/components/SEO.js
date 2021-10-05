d2ea7b8e0034705b31f2219fc9d8e2750df5b515
Joseph M <jmedina1985@hotmail.com>
2021-10-05 16:02:57 -0700

    SEO optimization

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,7 +3,12 @@ import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 const SEO = ({ title, description }) => {
-  return <Helmet title={title} meta={[{ name: `description` }]}></Helmet>
+  return (
+    <Helmet
+      title={title}
+      meta={[{ name: `description`, content: description }]}
+    ></Helmet>
+  )
 }
 
 export default SEO
