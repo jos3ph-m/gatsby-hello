fdb2e1dc899868303be2dde3677b2e26ac298d59
Joseph M <jmedina1985@hotmail.com>
2021-10-05 15:53:38 -0700

    SEO optimization

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import { Link, graphql } from "gatsby"
 import RecipesList from "../components/RecipesList"
+import SEO from "../components/SEO"
 
 const About = ({
   data: {
