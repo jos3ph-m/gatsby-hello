81d60993e914582b483d5b089bd06884477b12bf
Joseph M <jmedina1985@hotmail.com>
2021-10-06 15:15:20 -0700

    SEO optimization

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,7 @@ const Contact = ({ data }) => {
   const recipes = data.allContentfulRecipe.nodes
   return (
     <Layout>
+      <SEO title="Contact" />
       <main className="page">
         <section className="contact-page">
           <article className="contact-info">
