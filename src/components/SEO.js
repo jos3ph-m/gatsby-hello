c38cd09cde8a284c911d15035cf7b616d4bf238b
Joseph M <jmedina1985@hotmail.com>
2021-10-06 15:11:46 -0700

    SEO optimization

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -15,6 +15,7 @@ const query = graphql`
 
 const SEO = ({ title, description }) => {
   const { site } = useStaticQuery(query)
+  const metaDescription = description || site.siteMetadata.description
   return (
     <Helmet
       htmlAttributes={{ lang: "eng" }}
