7130a8dd297ea2820f7ea0b15b9c18941b7d8a4d
Joseph M <jmedina1985@hotmail.com>
2021-10-06 15:08:46 -0700

    SEO optimization

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,6 +2,17 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+const query = graphql`
+  {
+    site(siteMetadata: { title: {} }) {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
+
 const SEO = ({ title, description }) => {
   return (
     <Helmet
