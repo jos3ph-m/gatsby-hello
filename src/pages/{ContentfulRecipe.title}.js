c000ee9c86cfc4b57d7ddab17f3f2ec33dfc84e1
Joseph M <jmedina1985@hotmail.com>
2021-10-08 12:11:16 -0700

    SEO complete

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -13,6 +13,7 @@ const RecipeTemplate = ({ data }) => {
   const { tags, instructions, ingredients, tools } = content
   return (
     <Layout>
+      <SEO />
       <main className="page">
         <div className="recipe-page">
           {/* hero */}
