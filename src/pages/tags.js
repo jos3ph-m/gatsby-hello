df129be9c838bea94f33f7328ee797592d702401
Joseph M <jmedina1985@hotmail.com>
2021-10-08 12:09:42 -0700

    SEO complete

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout"
 import { graphql, Link } from "gatsby"
 import setupTags from "../utils/setupTags"
 import slugify from "slugify"
+import SEO from "../components/SEO"
 
 const Tags = ({ data }) => {
   const newTags = setupTags(data.allContentfulRecipe.nodes)
